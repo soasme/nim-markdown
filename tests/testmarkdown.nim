0a57ab595fb3111918c16664ad00f8eaba003758
soasme <soasme@gmail.com>
2018-10-22 16:25:41 +1300

    add no-|-ending table test case.

--- tests/testmarkdown.nim
+++ tests/testmarkdown.nim
@@ -188,3 +188,21 @@ test "table":
     <tr><td>Cell 5</td><td>Cell 6</td><td>Cell 7</td><td>Cell 8</td></tr>
   </tbody>
 </table>""".replace(re"\n *", "")
+
+  check markdown("""
+| Header 1 | Header 2
+| -------- | --------
+| Cell 1   | Cell 2
+| Cell 3   | Cell 4""") == """
+<table>
+  <thead>
+    <tr>
+      <th>Header 1</th>
+      <th>Header 2</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr><td>Cell 1</td><td>Cell 2</td></tr>
+    <tr><td>Cell 3</td><td>Cell 4</td></tr>
+  </tbody>
+</table>""".replace(re"\n *", "")
